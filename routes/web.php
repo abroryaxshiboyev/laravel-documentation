79a17767ccc16478a6bdce7f44ea382fa4855b41
Abror <abroryaxshiboyev2002@gmail.com>
2025-04-24 15:50:21 +0500

    Responses

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,35 @@ Route::get('/greeting', function () {
 });
 
 Route::get('/user/{id}', [UserController::class, 'show']);
+
+function streamedContent(): Generator {
+    yield 'Hello, ';
+    yield 'World!';
+}
+
+Route::get('/stream', function () {
+    return response()->stream(function (): void {
+        foreach (User::get() as $chunk) {
+            echo $chunk;
+            ob_flush();
+            flush();
+            sleep(2); // Simulate delay between chunks...
+        }
+    }, 200, ['X-Accel-Buffering' => 'no']);
+});
+
+Route::get('/users.json', function () {
+    return response()->streamJson([
+        'users' => User::cursor(),
+    ]);
+});
+
+Route::get('/chat', function () {
+    return response()->eventStream(function () {
+        $stream = OpenAI::client()->chat()->createStreamed(...);
+
+        foreach ($stream as $response) {
+            yield $response->choices[0];
+        }
+    });
+});
